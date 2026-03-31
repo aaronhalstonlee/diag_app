0a399a75cd9a69ff720da28146b9f989fa30bc0b
aaron <33557557+aaronhalstonlee@users.noreply.github.com>
2026-03-31 10:19:40 -0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'diagtree-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './manifest.json'
+];
+
+// Install Event: Cache all essential files
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Activate Event: Cleanup old caches if version changes
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Fetch Event: Serve from cache first, then network
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
